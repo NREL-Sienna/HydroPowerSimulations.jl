025c460f2cf6b55338f952218eab0ad65b201a38
Nongchao Guo <43042596+Nongchao@users.noreply.github.com>
2020-09-09 10:46:25 -0600

    create HydroPowerSimulations

--- src/HydroPowerSimulations.jl
+++ src/HydroPowerSimulations.jl
@@ -1,5 +1,34 @@
 module HydroPowerSimulations
 
-greet() = print("Hello World!")
+#################################################################################
+# Exports
+export HydroEnergyCascade
+export HydroDispatchReservoirCascade
+
+#################################################################################
+# Imports
+import Revise
+import PowerSystems
+import InfrastructureSystems
+import Dates
+import TimeZones
+import PowerSimulations
+import PowerModels
+import JuMP
+import Cbc
+import CSV
+import DataFrames
+
+const PSY = PowerSystems
+const IS = InfrastructureSystems
+const PM = PowerModels
+const PSI = PowerSimulations
+
+#################################################################################
+# Includes
+include("models/HydroEnergyCascade.jl")
+include("devices_models/devices/HydroReservoirCascade.jl")
+include("devices_models/device_constructors/HydroReservoirCascade_constructor.jl")
+include("parsers/set_upstream.jl")
 
 end # module
