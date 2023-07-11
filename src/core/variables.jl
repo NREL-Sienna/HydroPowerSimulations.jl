507a642ef8247cd9eb478e6e50afa1c8f49e35bd
Jose Daniel Lara <jdlara@berkeley.edu>
2023-07-11 15:33:27 -0600

    add variable correctly

--- src/core/variables.jl
+++ src/core/variables.jl
@@ -1,1 +1,8 @@
-struct WaterSpillageVariable <: PSI.VariableType end
+"""
+Struct to dispatch the creation of energy (water) spillage variable representing energy released from a storage/reservoir not injected into the network
+
+Docs abbreviation: ``S``
+"""
+struct WaterSpillageVariable <: VariableType end
+
+# convert_result_to_natural_units(::Type{WaterSpillageVariable }) = true # TODO: is this pu?
