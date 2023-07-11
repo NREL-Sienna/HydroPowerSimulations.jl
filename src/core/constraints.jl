f07923fae1640463d824e07bbc5cfedea75bc47a
Jose Daniel Lara <jdlara@berkeley.edu>
2023-07-11 15:52:51 -0600

    move more constraints

--- src/core/constraints.jl
+++ src/core/constraints.jl
@@ -2,5 +2,8 @@ struct EnergyLimitConstraint <: PSI.ConstraintType end
 struct EnergyTargetConstraint <: PSI.ConstraintType end
 struct EnergyShortageVariableLimitsConstraint <: PSI.ConstraintType end
 
-struct EnergyCapacityDownConstraint <: ConstraintType end
-struct EnergyCapacityUpConstraint <: ConstraintType end
+struct EnergyCapacityDownConstraint <: PSI.ConstraintType end
+struct EnergyCapacityUpConstraint <: PSI.ConstraintType end
+
+struct EnergyBudgetConstraint <: PSI.ConstraintType end
+struct EnergyCapacityConstraint <: PSI.ConstraintType end
