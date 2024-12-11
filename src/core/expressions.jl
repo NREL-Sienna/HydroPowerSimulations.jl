ec272404c8b3722a0eeaf67b24cb5e64a3f67203
rodrigomha <rodrigomha@gmail.com>
2024-12-11 15:03:05 -0800

    add export for expressions

--- src/core/expressions.jl
+++ src/core/expressions.jl
@@ -1,2 +1,5 @@
 struct ReserveRangeExpressionLB <: PSI.RangeConstraintLBExpressions end
 struct ReserveRangeExpressionUB <: PSI.RangeConstraintUBExpressions end
+
+PSI.should_write_resulting_value(::Type{ReserveRangeExpressionUB}) = true
+PSI.should_write_resulting_value(::Type{ReserveRangeExpressionLB}) = true
