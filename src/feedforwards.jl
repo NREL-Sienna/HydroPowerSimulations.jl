25d1d24db8f51958e8879dd0d5325e79481b2b4a
rodrigomha <rodrigomha@gmail.com>
2024-12-23 16:31:30 -0800

    add docstrings for FF

--- src/feedforwards.jl
+++ src/feedforwards.jl
@@ -1,12 +1,23 @@
 """
+    ReservoirTargetFeedforward(
+        component_type::Type{<:PSY.Component},
+        source::Type{T},
+        affected_values::Vector{DataType},
+        target_period::Int,
+        penalty_cost::Float64,
+        meta = CONTAINER_KEY_EMPTY_META
+    ) where {T}
+
 Adds a constraint to enforce a minimum reservoir level target with a slack
 variable associated with a penalty term.
 
-# Fields:
+# Arguments:
 
-    optmization_container_key ->
-    source - From where the data comes
-    affected_values -
+  - `component_type::Type{<:PSY.Component}` : Specify the type of component on which the Feedforward will be applied
+  - `source::Type{T}` : Specify the VariableType, ParameterType or AuxVariableType as the source of values for the Feedforward
+  - `affected_values::Vector{DataType}` : Specify the variable on which the reservoir target will be applied using the source values
+  - `target_period::Int` : Specify the time step at which the reservoir target will be applied.
+  - `penalty_cost::Float64` : Specify the penalty cost for not reaching the desired reservoir target.
 """
 struct ReservoirTargetFeedforward <: PSI.AbstractAffectFeedforward
     optimization_container_key::PSI.OptimizationContainerKey
@@ -131,7 +142,22 @@ function PSI._add_feedforward_arguments!(
 end
 
 """
-Adds a constraint to limit the sum of a variable over the number of periods to the source value
+    ReservoirLimitFeedforward(
+        component_type::Type{<:PSY.Component},
+        source::Type{T},
+        affected_values::Vector{DataType},
+        number_of_periods::Int,
+        meta = CONTAINER_KEY_EMPTY_META
+    ) where {T}
+
+Adds a constraint to limit the sum of a variable over the number of periods to the source value.
+
+# Arguments:
+
+  - `component_type::Type{<:PSY.Component}` : Specify the type of component on which the Feedforward will be applied
+  - `source::Type{T}` : Specify the VariableType, ParameterType or AuxVariableType as the source of values for the Feedforward
+  - `affected_values::Vector{DataType}` : Specify the variable on which the reservoir limit will be applied using the source values
+  - `number_of_periods::Int` : Specify the total number of periods that are added on which the limits are applied. For example, in a 24-step simulation if `number_of_periods = 24`, it will add only one constraint over the sum of 24 periods on which the reservoir limit will be applied. If `number_of_periods = 12`, it will create two constraints, one for the first 12 steps and one for the 13:24 steps. It is mandatory that `number_of_periods` can divide the total number of simulation steps.
 """
 struct ReservoirLimitFeedforward <: PSI.AbstractAffectFeedforward
     optimization_container_key::PSI.OptimizationContainerKey
