03d0a6a8baf7c3ed3f54e38e333821b0e9929e38
rodrigomha <rodrigomha@gmail.com>
2024-12-23 16:33:31 -0800

    add docstring for IC params and vars

--- src/core/initial_conditions.jl
+++ src/core/initial_conditions.jl
@@ -1,2 +1,9 @@
+"""
+Initial condition for Upper reservoir in HydroPumpedStorage formulations
+"""
 struct InitialHydroEnergyLevelUp <: PSI.InitialConditionType end
+
+"""
+Initial condition for Down reservoir in HydroPumpedStorage formulations
+"""
 struct InitialHydroEnergyLevelDown <: PSI.InitialConditionType end
