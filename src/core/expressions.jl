fae3da46ffc8cba7fe56ebc0e9c7b5af3b2bd86f
rodrigomha <rodrigomha@gmail.com>
2024-12-23 16:32:26 -0800

    update docstrings for expressions

--- src/core/expressions.jl
+++ src/core/expressions.jl
@@ -1,4 +1,12 @@
+"""
+Expression for HydroPumpedStorage that keep track
+of active power and reserves for Lower Bound limits
+"""
 struct ReserveRangeExpressionLB <: PSI.RangeConstraintLBExpressions end
+"""
+Expression for HydroPumpedStorage that keep track
+of active power and reserves for Upper Bound limits
+"""
 struct ReserveRangeExpressionUB <: PSI.RangeConstraintUBExpressions end
 
 PSI.should_write_resulting_value(::Type{ReserveRangeExpressionUB}) = true
