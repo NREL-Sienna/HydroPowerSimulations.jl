9b034f0e8aed3eff9f00c6a54807984ba7990e0c
rodrigomha <rodrigomha@gmail.com>
2024-12-31 11:13:47 -0800

    update formatter

--- scripts/formatter/formatter_code.jl
+++ scripts/formatter/formatter_code.jl
@@ -1,41 +1,30 @@
 using Pkg
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
+Pkg.update()
 using JuliaFormatter
 
-main_paths = [".", "./docs/src"]
+main_paths = ["./src", "./test", "./docs/src"]
 for main_path in main_paths
-    format(
-        main_path;
-        whitespace_ops_in_indices=true,
-        remove_extra_newlines=true,
-        verbose=true,
-        always_for_in=true,
-        whitespace_typedefs=true,
-        whitespace_in_kwargs=false,
-        format_docstrings=true,
-        always_use_return=false, # removed since it has false positives.
-    )
-end
+    for (root, dir, files) in walkdir(main_path)
+        for f in files
+        @show file_path = abspath(root, f)
+        !((occursin(".jl", f) || occursin(".md", f))) && continue
+        format(file_path;
+            whitespace_ops_in_indices = true,
+            remove_extra_newlines = true,
+            verbose = true,
+            always_for_in = true,
+            whitespace_typedefs = true,
+            conditional_to_if = true,
+            join_lines_based_on_source = true,
+            separate_kwargs_with_semicolon = true,
+            format_markdown = true,
+            # Brackets in code blocks cause formatter to fail, ignore for now
+            ignore = ["style.md", "index.md", "tests.md"],
 
-# Documentation Formatter
-main_paths = ["./docs/src"]
-for main_path in main_paths
-    for folder in readdir(main_path)
-        @show folder_path = joinpath(main_path, folder)
-        if isfile(folder_path)
-            !occursin(".md", folder_path) && continue
+            # always_use_return = true. # Disabled since it throws a lot of false positives
+            )
         end
-        format(
-            folder_path;
-            format_markdown=true,
-            whitespace_ops_in_indices=true,
-            remove_extra_newlines=true,
-            verbose=true,
-            always_for_in=true,
-            whitespace_typedefs=true,
-            whitespace_in_kwargs=false,
-            # always_use_return = true # removed since it has false positives.
-        )
     end
 end
