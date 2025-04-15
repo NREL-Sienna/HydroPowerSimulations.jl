d808bf6bf5a4942a775d8c18a0f7cb58e749d1dc
Abdelrahman Ayad <abdelrahman.ayad@nrel.gov>
2025-04-14 20:49:29 -0600

    minor fix to `test_block_optimization.jl`

--- test/test_block_optimization.jl
+++ test/test_block_optimization.jl
@@ -1,7 +1,6 @@
 @testset "Hydro Block Optimization Begin" begin
     output_dir = mktempdir(; cleanup = true)
-    device_model = PSI.DeviceModel(HydroEnergyReservoir, HydroCommitmentReservoirBudget)
-
+    
     modeling_horizon = 52 * 24 * 1
     sys = get_test_reservoir_turbine_sys(modeling_horizon)
 
