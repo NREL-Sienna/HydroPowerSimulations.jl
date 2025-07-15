5fabc45eb4a8fc301af048daaa3be0421f989fd1
Mia Moore <198999385+mmoore-MEM@users.noreply.github.com>
2025-07-15 14:13:57 -0600

    tests for TOTAL_VOLUME and HEAD single stage decision problems

--- test/test_single_stage_hydro_models.jl
+++ test/test_single_stage_hydro_models.jl
@@ -186,3 +186,191 @@ set_device_model!(template_ed, ThermalStandard, ThermalBasicDispatch)
     #@test execute!(sim; enable_progress_bar = false) == PSI.RunStatus.SUCCESSFULLY_FINALIZED
 
 end
+
+
+# Reservoir data in VOLUME
+@testset "Hydro Reservoir Data in TOTAL_VOLUME, single step simulation" begin
+# key word args for custom test system
+PSB = PowerSystemCaseBuilder
+kwargs = (
+        withStandardLoad=true,
+        withThermalStandard=true,
+        withRenewableDispatch=true,
+        withRenewableNonDispatch=true,
+        withEnergyReservoirStorage=true,
+        withInterruptiblePowerLoad=true,
+        withHydroTurbine=true,
+        withHydroPumpTurbine=false,
+        withHydroDispatch=true,
+        hydroLevelDataType=PSY.ReservoirDataType.TOTAL_VOLUME
+    )
+test_system_uc = PSB.build_system(PSISystems,"csys5_custom";skip_serialization=true,time_series_in_memory=true,decision_model_type="uc",kwargs...)
+test_system_ed = PSB.build_system(PSISystems,"csys5_custom";skip_serialization=true,time_series_in_memory=true,decision_model_type="uc",kwargs...)
+
+# set templates and device models
+template_uc = ProblemTemplate(NetworkModel(CopperPlatePowerModel))
+set_device_model!(template_uc, HydroReservoir, HydroWaterModelReservoir)
+set_device_model!(template_uc, HydroTurbine, HydroTurbineBilinearDispatch)
+set_device_model!(template_uc, ThermalStandard, ThermalStandardUnitCommitment)
+template_ed = ProblemTemplate(NetworkModel(CopperPlatePowerModel))
+set_device_model!(template_ed, HydroReservoir, HydroWaterModelReservoir)
+set_device_model!(template_ed, HydroTurbine, HydroTurbineBilinearDispatch)
+set_device_model!(template_ed, ThermalStandard, ThermalStandardDispatch)
+# TODO: Test HydroPumpTurbine formulation?
+
+# see if single decision models will build
+
+# Set up Simulation
+    models = SimulationModels(;
+        decision_models = [
+            DecisionModel(
+                template_uc,
+                test_system_uc;
+                name = "UC",
+                optimizer = SCIP.Optimizer,
+                system_to_file = false,
+                initialize_model = true,
+                optimizer_solve_log_print = false,
+                direct_mode_optimizer = true,
+                rebuild_model = false,
+                store_variable_names = true,
+                #check_numerical_bounds=false,
+            ),
+            DecisionModel(
+                template_ed,
+                test_system_ed;
+                name = "ED",
+                optimizer = SCIP.Optimizer,
+                system_to_file = false,
+                initialize_model = true,
+                optimizer_solve_log_print = false,
+                check_numerical_bounds = false,
+                rebuild_model = false,
+                calculate_conflict = true,
+                store_variable_names = true,
+                #export_pwl_vars = true,
+            ),
+        ],
+    )
+
+    sequence = SimulationSequence(;
+        models = models,
+        feedforwards = Dict(
+        ),
+        ini_cond_chronology = InterProblemChronology(),
+    )
+
+    using Dates
+    sim = Simulation(;
+        name = "HydroTest",
+        steps = 2,
+        models = models,
+        sequence = sequence,
+        initial_time = DateTime("2020-01-01T00:00:00"),
+        simulation_folder = mktempdir(; cleanup = true),
+    )
+
+    problem = DecisionModel(template_uc, test_system_uc; optimizer = SCIP.Optimizer, horizon = Hour(24))
+    @test build!(problem; output_dir = mktempdir()) == InfrastructureSystems.Optimization.ModelBuildStatusModule.ModelBuildStatus.BUILT
+
+    problem = DecisionModel(template_ed, test_system_ed; optimizer = SCIP.Optimizer, horizon = Hour(24))
+    @test build!(problem; output_dir = mktempdir()) == InfrastructureSystems.Optimization.ModelBuildStatusModule.ModelBuildStatus.BUILT
+     
+    #@test build!(sim; serialize = false) == PSI.SimulationBuildStatus.BUILT
+    #@test execute!(sim; enable_progress_bar = false) == PSI.RunStatus.SUCCESSFULLY_FINALIZED
+
+end
+
+
+# Reservoir data in HEAD
+@testset "Hydro Reservoir Data in HEAD, single step simulation" begin
+# key word args for custom test system
+PSB = PowerSystemCaseBuilder
+kwargs = (
+        withStandardLoad=true,
+        withThermalStandard=true,
+        withRenewableDispatch=true,
+        withRenewableNonDispatch=true,
+        withEnergyReservoirStorage=true,
+        withInterruptiblePowerLoad=true,
+        withHydroTurbine=true,
+        withHydroPumpTurbine=false,
+        withHydroDispatch=true,
+        hydroLevelDataType=PSY.ReservoirDataType.HEAD
+    )
+test_system_uc = PSB.build_system(PSISystems,"csys5_custom";skip_serialization=true,time_series_in_memory=true,decision_model_type="uc",kwargs...)
+test_system_ed = PSB.build_system(PSISystems,"csys5_custom";skip_serialization=true,time_series_in_memory=true,decision_model_type="uc",kwargs...)
+
+# set templates and device models
+template_uc = ProblemTemplate(NetworkModel(CopperPlatePowerModel))
+set_device_model!(template_uc, HydroReservoir, HydroWaterModelReservoir)
+set_device_model!(template_uc, HydroTurbine, HydroTurbineBilinearDispatch)
+set_device_model!(template_uc, ThermalStandard, ThermalStandardUnitCommitment)
+template_ed = ProblemTemplate(NetworkModel(CopperPlatePowerModel))
+set_device_model!(template_ed, HydroReservoir, HydroWaterModelReservoir)
+set_device_model!(template_ed, HydroTurbine, HydroTurbineBilinearDispatch)
+set_device_model!(template_ed, ThermalStandard, ThermalStandardDispatch)
+# TODO: Test HydroPumpTurbine formulation?
+
+# see if single decision models will build
+
+# Set up Simulation
+    models = SimulationModels(;
+        decision_models = [
+            DecisionModel(
+                template_uc,
+                test_system_uc;
+                name = "UC",
+                optimizer = SCIP.Optimizer,
+                system_to_file = false,
+                initialize_model = true,
+                optimizer_solve_log_print = false,
+                direct_mode_optimizer = true,
+                rebuild_model = false,
+                store_variable_names = true,
+                #check_numerical_bounds=false,
+            ),
+            DecisionModel(
+                template_ed,
+                test_system_ed;
+                name = "ED",
+                optimizer = SCIP.Optimizer,
+                system_to_file = false,
+                initialize_model = true,
+                optimizer_solve_log_print = false,
+                check_numerical_bounds = false,
+                rebuild_model = false,
+                calculate_conflict = true,
+                store_variable_names = true,
+                #export_pwl_vars = true,
+            ),
+        ],
+    )
+
+    sequence = SimulationSequence(;
+        models = models,
+        feedforwards = Dict(
+        ),
+        ini_cond_chronology = InterProblemChronology(),
+    )
+
+    using Dates
+    sim = Simulation(;
+        name = "HydroTest",
+        steps = 2,
+        models = models,
+        sequence = sequence,
+        initial_time = DateTime("2020-01-01T00:00:00"),
+        simulation_folder = mktempdir(; cleanup = true),
+    )
+
+    problem = DecisionModel(template_uc, test_system_uc; optimizer = SCIP.Optimizer, horizon = Hour(24))
+    @test build!(problem; output_dir = mktempdir()) == InfrastructureSystems.Optimization.ModelBuildStatusModule.ModelBuildStatus.BUILT
+
+    problem = DecisionModel(template_ed, test_system_ed; optimizer = SCIP.Optimizer, horizon = Hour(24))
+    @test build!(problem; output_dir = mktempdir()) == InfrastructureSystems.Optimization.ModelBuildStatusModule.ModelBuildStatus.BUILT
+     
+    #@test build!(sim; serialize = false) == PSI.SimulationBuildStatus.BUILT
+    #@test execute!(sim; enable_progress_bar = false) == PSI.RunStatus.SUCCESSFULLY_FINALIZED
+
+end
