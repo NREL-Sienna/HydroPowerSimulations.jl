ef4e119eafa0a3151f66c7dd190d05b84576b75e
rodrigomha <rodrigomha@gmail.com>
2025-07-17 12:31:57 -0700

    add new test data

--- test/data_utils/new_reservoir_test_sys.jl
+++ test/data_utils/new_reservoir_test_sys.jl
@@ -1,5 +1,5 @@
-function new_c_sys5_hyd()
-    sys = PSB.build_system(PSITestSystems, "c_sys5_hyd")
+function new_c_sys5_hyd(; with_reserves = false)
+    sys = PSB.build_system(PSITestSystems, "c_sys5_hyd"; add_reserves = with_reserves)
 
     hy_res = first(PSY.get_components(HydroEnergyReservoir, sys))
 
@@ -47,7 +47,46 @@ function new_c_sys5_hyd()
     add_component!(sys, turbine_res)
     add_component!(sys, reservoir_res)
     set_reservoirs!(turbine_res, [reservoir_res])
+    for s in get_components(Service, sys)
+        add_service!(turbine_res, s, sys)
+    end
 
     copy_time_series!(reservoir_res, hy_res)
+    return sys
+end
+
+function new_c_sys5_hyd_dispatch(; with_reserves = false)
+    sys = PSB.build_system(PSITestSystems, "c_sys5_hyd"; add_reserves = with_reserves) # Need to add reserves for c_sys5_hy
+
+    hy_res = first(PSY.get_components(HydroEnergyReservoir, sys))
+
+    hy_dispatch = HydroDispatch(;
+        name = "$(get_name(hy_res))",
+        available = true,
+        bus = get_bus(hy_res),
+        active_power = 0.0,
+        reactive_power = 0.0,
+        rating = hy_res.rating,
+        prime_mover_type = hy_res.prime_mover_type,
+        active_power_limits = (
+            min = hy_res.active_power_limits.min,
+            max = hy_res.active_power_limits.max,
+        ),
+        reactive_power_limits = (
+            min = hy_res.reactive_power_limits.min,
+            max = hy_res.reactive_power_limits.max,
+        ),
+        ramp_limits = nothing,
+        time_limits = nothing,
+        base_power = 100.0,
+        operation_cost = get_operation_cost(hy_res),
+    )
+
+    add_component!(sys, hy_dispatch)
+    for s in get_components(Service, sys)
+        add_service!(hy_dispatch, s, sys)
+    end
+
+    copy_time_series!(hy_dispatch, hy_res)
     return sys
 end
